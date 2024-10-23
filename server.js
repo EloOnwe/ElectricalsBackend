ba4f95bd1c8c5d8d03852d0f8df2a741b4ddaf9b
Onwe Michael <elonwem@gmail.com>
2024-10-23 18:06:30 +0100

    updating serverjs

--- server.js
+++ server.js
@@ -15,37 +15,13 @@ const orderRoute = require("./routes/orderRoute");
 const app = express();
 require("dotenv").config();
 
-const apiBaseUrl =
-  process.env.NODE_ENV === "production"
-    ? process.env.CLIENT_URL // Production backend URL
-    : "http://localhost:5173"; // Local development URL
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://mickielect.onrender.com",
-];
-
-app.use(cors());
-app.use(express.static(path.join(__dirname, "build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true, // Enable cookies and other credentials
+    origin: "https://mickielect.onrender.com" || "http://localhost:5173/", // Replace with your frontend's origin
+    credentials: true, // Allow credentials (cookies, headers)
   })
 );
 
