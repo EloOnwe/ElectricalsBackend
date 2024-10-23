9aeec28a3989ab44c4a6b1a2a0a74c9dd4d6c38a
Onwe Michael <elonwem@gmail.com>
2024-10-23 12:15:30 +0100

    update forntend url

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // or whatever your frontend origin is
+    origin: process.env.CLIENT_URL || "http://localhost:5173", // or whatever your frontend origin is
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
