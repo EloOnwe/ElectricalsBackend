a482acb6fb268a9b6883b6b897dd044ed60a6197
Onwe Michael <elonwem@gmail.com>
2024-10-28 08:18:52 +0000

    update server

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const path = require("path");
 
 const connectToDB = require("./dbConnection/dbConnect");
 const userRoute = require("./routes/userRoute");
@@ -32,9 +31,10 @@ app.use(stripeRoute);
 app.use(webhookRoute);
 app.use(orderRoute);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Not Found" });
 });
+
 // app.use(errorHandler);
 const port = process.env.PORT || 5000;
 
